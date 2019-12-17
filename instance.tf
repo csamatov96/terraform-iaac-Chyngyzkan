9c300c3927cf5eb80e4e02379c6525e1f038a0b0
csamatov96 <csamatov96@gmail.com>
2019-12-16 20:27:35 -0600

    interpolation key attached to ec2

--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2" {
   ami                         = "${var.ami}" 
   instance_type               = "${var.instance_type}" 
   associate_public_ip_address = "${var.associate_public_ip_address}" 
+  key_name                    = "${aws_key_pair.keypair.key_name}" #interpolation added 
 
  tags = { 
     Name                      = "${var.tagEc2Name}" 
--- key-pair.tf
+++ key-pair.tf
@@ -5,5 +5,5 @@
 
 resource "aws_key_pair" "keypair" {  #no interpolate next to the resource name 
   key_name = "${var.key_name}" 
-  public_key = "${file("${var.key_name_location}")}" 
+  public_key = "${file("${var.key_name_location}")}" #interpolation = "${aws_key_pair.deployer.key_name}"
 } 
