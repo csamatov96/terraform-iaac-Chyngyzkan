d0551c7db4e2576470c02a39d2b5870e7ba7745d
csamatov96 <csamatov96@gmail.com>
2019-12-16 20:24:50 -0600

    vpc and sec group added

--- instance.tf
+++ instance.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "ec2" {
   associate_public_ip_address = "${var.associate_public_ip_address}" 
 
  tags = { 
-    Name = "${var.tagEc2Name}" 
+    Name                      = "${var.tagEc2Name}" 
   } 
 } 
--- variables.tf
+++ variables.tf
@@ -1,12 +1,18 @@
+
+#bucket
 variable "bucket" {} 
 variable "key_name" {} 
 
+#key
 variable  "key_name_location" {} 
 
+#ec2 
 variable "ami" {}
 variable "instance_type" {}
 variable "associate_public_ip_address" {}
 variable "tagEc2Name" {}
+
+variable "tagVPCname" {}
 
 
 
