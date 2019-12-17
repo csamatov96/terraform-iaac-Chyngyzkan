ad5f3a0d073a92a8178a4bff3d5c64641fa75705
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-16 19:18:32 -0600

    versioning added

--- s3.tf
+++ s3.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-class1" 
   acl    = "private" 
 
+  versioning { 
+    enabled = true 
+  } 
+
+
   tags = { 
     Name        = "My bucket" 
     Environment = "Dev" 
