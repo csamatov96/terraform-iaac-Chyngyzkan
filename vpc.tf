d1cd464a91415c9ef249dddcd1b72b5e9f98366c
csamatov96 <csamatov96@gmail.com>
2019-12-16 20:31:38 -0600

    subnet added

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "vpc" {
     Name = "${var.tagVPCname}"
   }
 }
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
