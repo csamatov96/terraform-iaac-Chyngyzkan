05ea506a0dbbac64ce6a7e1e076d3f22dfa2e54a
csamatov96 <csamatov96@gmail.com>
2019-12-16 20:37:39 -0600

    changes

--- instance.tf
+++ instance.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "ec2" {
   instance_type               = "${var.instance_type}" 
   associate_public_ip_address = "${var.associate_public_ip_address}" 
   key_name                    = "${aws_key_pair.keypair.key_name}" #interpolation added 
+  user_data = file("userdata.sh")
 
  tags = { 
     Name                      = "${var.tagEc2Name}" 
