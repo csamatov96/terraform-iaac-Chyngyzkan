aab3ae185e0977087716181a8629eba1708dbce6
csamatov96 <csamatov96@gmail.com>
2019-12-16 20:32:32 -0600

    lb added

--- lb.tf
+++ lb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "appLB" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.sg.id}"] #
-  subnets            = ["${aws_subnet.public.*.id}"]
+  subnets            = ["${aws_subnet.subnet.*.id}"]
 
   enable_deletion_protection = true
 
