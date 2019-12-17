0d9ce91f5a4538eeee170d23ba28029c5feb99f0
csamatov96 <csamatov96@gmail.com>
2019-12-16 20:10:55 -0600

    changes

--- key-pair.tf
+++ key-pair.tf
@@ -3,7 +3,7 @@
 #  public_key = file("~/.ssh/id_rsa.pub") 
 #}
 
-resource "aws_key_pair" "${var.key_name}" { 
-  key_name = "{var.key_name}" 
+resource "aws_key_pair" "keypair" { 
+  key_name = "${var.key_name}" 
   public_key = "${file("${var.key_name_location}")}" 
 } 
