52c4191e554025d8c39fab7fbf38860eb6c9c8e3
csamatov96 <csamatov96@gmail.com>
2019-12-18 19:07:50 -0600

    output added

--- output.tf
+++ output.tf
@@ -6,3 +6,17 @@ output "instance_ip_addr" {
     description = "The private IP address of the main server instance."
 }
 
+output "this_security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.allow_tls.name #name of sg to refer to 
+}
+
+output "key_name" {
+  description = "KEY"
+  value       = aws_key_pair.Terraform centos.key_name #name of the key to refer to 
+}
+
+output "username" {
+  value       = "ec2"
+}
+
