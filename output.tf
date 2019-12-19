072c0846274a11098a740ff522dea828d3b96b2d
csamatov96 <csamatov96@gmail.com>
2019-12-18 19:01:51 -0600

    changes

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
-output "bucketName" { 
-    value = aws_s3_bucket.s3bucket.bucket 
-} 
+#output "bucketName" { 
+#    value = aws_s3_bucket.s3bucket.bucket 
+#} 
+output "instance_ip_addr" {
+    value = aws_instance.ec2.private_ip
+    description = "The private IP address of the main server instance."
+}
+
