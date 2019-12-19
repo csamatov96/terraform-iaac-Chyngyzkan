5733bf7d8547eb005548604687bd1b5d7dde7135
csamatov96 <csamatov96@gmail.com>
2019-12-18 18:59:57 -0600

    output added

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "backend-state-chyngyzkan-class-two"
-    key    = "infrastructure_folder_class2"
+    key    = "infrastructure_folder_class2" #is how u wanna save u file 
     region = "us-east-1"
   }
 }
