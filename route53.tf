674af31c390c71ce2d9015e4d0a3ac1d84a86aef
csamatov96 <csamatov96@gmail.com>
2019-12-18 19:21:43 -0600

    changes

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" { 
   zone_id = "Z6XWWD56QDJWX"  #hosted zone of ur domain 
-  name    = "www.ec2.csamatov.net" 
+  name    = "ec2.csamatov.net" 
   type    = "A" 
   ttl     = "60" 
   records = ["aws_instance.ec2.public_ip"] #refering to the name of the resource 
